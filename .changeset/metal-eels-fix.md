d8fdcaa7aa4e7fbcd8e8c7b833dd1e8f7143af12
Zane Wolfgang Pickett <sirwolfgang@users.noreply.github.com>
2023-04-27 22:15:17 +0900

    Add option to set QuilttProvider to reset on session change

---
+++ .changeset/metal-eels-fix.md
@@ -0,0 +1,6 @@
+---
+"@quiltt/react": patch
+"@quiltt/core": patch
+---
+
+Add option to set QuilttProvider to reset on session change
--- ECMAScript/react/src/providers/QuilttAuthProvider.tsx
+++ ECMAScript/react/src/providers/QuilttAuthProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FC, PropsWithChildren } from 'react'
+import { FC, PropsWithChildren, useState } from 'react'
 import { useEffect } from 'react'
 
 import { ApolloProvider } from '@apollo/client/index.js'
@@ -8,6 +8,7 @@ import { InMemoryCache, QuilttClient } from '@quiltt/core'
 import { useQuilttSession } from '../hooks'
 
 type QuilttAuthProviderProps = PropsWithChildren & {
+  resetOnSessionChange?: boolean
   token?: string
 }
 
@@ -22,8 +23,14 @@ const Client = new QuilttClient({
  * from triggering within the transitionary state.
  *
  */
-export const QuilttAuthProvider: FC<QuilttAuthProviderProps> = ({ token, children }) => {
+export const QuilttAuthProvider: FC<QuilttAuthProviderProps> = ({
+  resetOnSessionChange, // If enabled, skips rendering children for a frame to trigger a reset
+  token,
+  children,
+}) => {
   const { session, importSession } = useQuilttSession()
+  const [isResetting, setIsResetting] = useState(false)
+  const [isResettingRenders, setIsResettingRenders] = useState(0)
 
   // Import Passed in Tokens
   useEffect(() => {
@@ -33,11 +40,22 @@ export const QuilttAuthProvider: FC<QuilttAuthProviderProps> = ({ token, childre
 
   // Reset Client Store when logging in or out
   useEffect(() => {
-    Client.resetStore()
+    setIsResetting(true)
+    setIsResettingRenders(2) // two is to ensure that 2 passes, or 1 frame is rendered null
+    Client.resetStore().then(() => setIsResetting(false))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session])
 
-  return <ApolloProvider client={Client}>{children}</ApolloProvider>
+  // Ensure that at least 1 render renders out null
+  if (isResettingRenders > 0) {
+    setIsResettingRenders((count) => count - 1)
+  }
+
+  return (
+    <ApolloProvider client={Client}>
+      {resetOnSessionChange && (isResetting || isResettingRenders > 0) ? null : children}
+    </ApolloProvider>
+  )
 }
 
 export default QuilttAuthProvider
