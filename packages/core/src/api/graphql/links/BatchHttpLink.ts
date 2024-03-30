9a7da60b9b0f6b5d2022f7831120f49f6b93a9f5
Zubair Aziz <zubairaziz.dev@gmail.com>
2024-03-30 15:32:10 +0800

    Favor native fetch whenever possible

--- packages/core/src/api/graphql/links/BatchHttpLink.ts
+++ packages/core/src/api/graphql/links/BatchHttpLink.ts
@@ -1,12 +1,14 @@
 import { BatchHttpLink as ApolloHttpLink } from '@apollo/client/link/batch-http/index.js'
+import crossfetch from 'cross-fetch'
 
-import fetch from 'cross-fetch'
+// Use `cross-fetch` only if `fetch` is not available on the `globalThis` object
+const effectiveFetch = typeof fetch === 'undefined' ? crossfetch : fetch
 
 import { endpointGraphQL } from '../../../configuration'
 
 export const BatchHttpLink = new ApolloHttpLink({
   uri: endpointGraphQL,
-  fetch,
+  fetch: effectiveFetch,
 })
 
 export default BatchHttpLink
