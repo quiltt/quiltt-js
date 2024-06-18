486ab04b788fb12bb5b19f0a51a5ca95a162826e
Zubair <zubairaziz.dev@gmail.com>
2024-06-19 05:50:41 +0800

    [react-native] Refactor "QuilttConnector" for better visibility (#266)
    * [react-native] Refactor "QuilttConnector" for better visibility
    
    * Refactor checkConnectorUrl
    
    * Update tests
    
    * Revert 'Linking' module
    
    * Update pnpm/action-setup

--- packages/react-native/src/components/QuilttConnector.tsx
+++ packages/react-native/src/components/QuilttConnector.tsx
@@ -1,10 +1,8 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-
-// React Native's URL implementation is incomplete
-// https://github.com/facebook/react-native/issues/16434
-import { URL } from 'react-native-url-polyfill'
+import { URL } from 'react-native-url-polyfill' // https://github.com/facebook/react-native/issues/16434
 import { WebView } from 'react-native-webview'
 import type { ShouldStartLoadRequest } from 'react-native-webview/lib/WebViewTypes'
+import { Linking, Platform } from 'react-native'
 
 import {
   ConnectorSDKCallbackMetadata,
@@ -13,12 +11,56 @@ import {
   useQuilttSession,
 } from '@quiltt/react'
 
+import { getErrorMessage, ErrorReporter } from '../utils'
 import { version } from '../version'
 import { AndroidSafeAreaView } from './AndroidSafeAreaView'
 import { ErrorScreen } from './ErrorScreen'
 import { LoadingScreen } from './LoadingScreen'
-import { checkConnectorUrl, handleOAuthUrl } from '../utils'
-import type { PreFlightCheck } from '../utils'
+
+const errorReporter = new ErrorReporter(`${Platform.OS} ${Platform.Version}`)
+const PREFLIGHT_RETRY_COUNT = 3
+
+export type PreFlightCheck = {
+  checked: boolean
+  error?: string
+}
+
+export const checkConnectorUrl = async (
+  connectorUrl: string,
+  retryCount = 0
+): Promise<PreFlightCheck> => {
+  let responseStatus
+  let error
+  try {
+    const response = await fetch(connectorUrl)
+    if (!response.ok) {
+      responseStatus = response.status
+      throw new Error(`The URL ${connectorUrl} is not routable.`)
+    }
+    console.log(`The URL ${connectorUrl} is routable.`)
+    return { checked: true }
+  } catch (e) {
+    error = e
+    console.error(`An error occurred while checking the connector URL: ${error}`)
+
+    if (retryCount < PREFLIGHT_RETRY_COUNT) {
+      const delay = 50 * Math.pow(2, retryCount)
+      await new Promise((resolve) => setTimeout(resolve, delay))
+      console.log(`Retrying... Attempt number ${retryCount + 1}`)
+      return checkConnectorUrl(connectorUrl, retryCount + 1)
+    }
+    const errorMessage = getErrorMessage(responseStatus, error as Error)
+    const errorToSend = (error as Error) || new Error(errorMessage)
+    const context = { connectorUrl, responseStatus }
+    if (responseStatus !== 404) await errorReporter.send(errorToSend, context)
+    return { checked: true, error: errorMessage }
+  }
+}
+
+export const handleOAuthUrl = (oauthUrl: URL | string) => {
+  console.log(`handleOAuthUrl - Opening URL - ${oauthUrl.toString()}`)
+  Linking.openURL(oauthUrl.toString())
+}
 
 type QuilttConnectorProps = {
   testId?: string
@@ -43,17 +85,20 @@ const QuilttConnector = ({
 }: QuilttConnectorProps) => {
   const webViewRef = useRef<WebView>(null)
   const { session } = useQuilttSession()
+
   const encodedOAuthRedirectUrl = useMemo(
     () => encodeURIComponent(oauthRedirectUrl),
     [oauthRedirectUrl]
   )
+
   const connectorUrl = useMemo(() => {
-    const url: URL = new URL(`https://${connectorId}.quiltt.app`)
+    const url = new URL(`https://${connectorId}.quiltt.app`)
     url.searchParams.append('mode', 'webview')
     url.searchParams.append('oauth_redirect_url', encodedOAuthRedirectUrl)
     url.searchParams.append('agent', `react-native-${version}`)
     return url.toString()
   }, [connectorId, encodedOAuthRedirectUrl])
+
   const [preFlightCheck, setPreFlightCheck] = useState<PreFlightCheck>({ checked: false })
 
   useEffect(() => {
@@ -86,35 +131,9 @@ const QuilttConnector = ({
     webViewRef.current?.injectJavaScript(script)
   }, [connectionId, connectorId, institution, session?.token])
 
-  // urlAllowList & shouldRender ensure we are only rendering Quiltt, MX and Plaid content in Webview
-  // For other urls, we assume those are bank urls, which need to be handled in external browser.
-  // TODO: Need to regroup on this and figure out a better way to handle a URL allow list
-  // const urlAllowList = useMemo(
-  //   () => [
-  //     'quiltt.io',
-  //     'quiltt.app',
-  //     'quiltt.dev',
-  //     'moneydesktop.com',
-  //     'plaid.com',
-  //     'https://cdn.plaid.com/link',
-  //     'https://www.google.com/recaptcha',
-  //     'https://challenges.cloudflare.com',
-  //     'https://api.stripe.com',
-  //     'https://cdn.jsdelivr.net',
-  //     'https://auth0.com',
-  //   ],
-  //   []
-  // )
-
   const isQuilttEvent = useCallback((url: URL) => url.protocol === 'quilttconnector:', [])
 
-  const shouldRender = useCallback(
-    (url: URL) => {
-      if (isQuilttEvent(url)) return false
-      return url.protocol === 'https:'
-    },
-    [isQuilttEvent]
-  )
+  const shouldRender = useCallback((url: URL) => !isQuilttEvent(url), [isQuilttEvent])
 
   const clearLocalStorage = () => {
     const script = 'localStorage.clear();'
@@ -193,7 +212,7 @@ const QuilttConnector = ({
   )
 
   if (!preFlightCheck.checked) return <LoadingScreen testId="loading-screen" />
-  if (preFlightCheck.error)
+  if (preFlightCheck.error) {
     return (
       <ErrorScreen
         testId="error-screen"
@@ -201,6 +220,7 @@ const QuilttConnector = ({
         cta={() => onExitError?.({ connectorId })}
       />
     )
+  }
 
   return (
     <AndroidSafeAreaView testId={testId}>
--- packages/react-native/src/utils/connector/handleOAuthUrl.ts
+++
@@ -1,9 +0,0 @@
-import { Linking } from 'react-native'
-
-export const handleOAuthUrl = (oauthUrl: URL) => {
-  if (oauthUrl.protocol !== 'https:') {
-    console.log(`handleOAuthUrl - Skipping non https url - ${oauthUrl.href}`)
-    return
-  }
-  Linking.openURL(oauthUrl.href)
-}
