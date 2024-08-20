2e9c962ee0739f621644b114adfbab9c5b057005
zubairaziz <zubairaziz.dev@gmail.com>
2024-08-21 04:57:32 +0800

    Update tests

--- examples/react-nextjs/next.config.js
+++ examples/react-nextjs/next.config.js
@@ -1,16 +1,7 @@
-import NodePolyfillPlugin from 'node-polyfill-webpack-plugin'
-
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
   transpilePackages: ['@quiltt/react', '@apollo/client'],
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.plugins.push(new NodePolyfillPlugin())
-    }
-
-    return config
-  },
 }
 
 export default config
