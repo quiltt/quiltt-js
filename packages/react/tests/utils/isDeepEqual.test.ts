f37e6fe9e9f33bb30e4c840992c80ef3bd05e4bf
zubairaziz <zubairaziz.dev@gmail.com>
2024-12-20 03:16:05 +0800

    Run linter & formatter

--- packages/react/tests/utils/isDeepEqual.test.ts
+++ packages/react/tests/utils/isDeepEqual.test.ts
@@ -1,5 +1,5 @@
-import { describe, it, expect } from 'vitest'
 import { isDeepEqual } from '@/utils'
+import { describe, expect, it } from 'vitest'
 
 describe('isDeepEqual', () => {
   // Primitive types
