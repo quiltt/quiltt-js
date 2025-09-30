38a2c04048e6a37c565488cf2f892bd540714944
Ruben Izmailyan <ruben@quiltt.io>
2025-09-30 14:00:32 -0500

    Fix import name

--- packages/core/src/api/graphql/links/BatchHttpLink.ts
+++ packages/core/src/api/graphql/links/BatchHttpLink.ts
@@ -1,4 +1,4 @@
-import { BatchHttpLink as ApolloHttpLink } from '@apollo/client/link/batch-http/index.js'
+import { BatchHttpLink as ApolloBatchHttpLink } from '@apollo/client/link/batch-http/index.js'
 import crossfetch from 'cross-fetch'
 
 import { endpointGraphQL } from '@/configuration'
@@ -6,7 +6,7 @@ import { endpointGraphQL } from '@/configuration'
 // Use `cross-fetch` only if `fetch` is not available on the `globalThis` object
 const effectiveFetch = typeof fetch === 'undefined' ? crossfetch : fetch
 
-export const BatchHttpLink = new ApolloHttpLink({
+export const BatchHttpLink = new ApolloBatchHttpLink({
   uri: endpointGraphQL,
   fetch: effectiveFetch,
 })
