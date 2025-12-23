81a049e983b48cffd0abe2244ec5575fd7d5746e
zubairaziz <zubairaziz.dev@gmail.com>
2025-12-23 17:01:42 +0700

    Fix packaging options

--- examples/react-native-expo/plugins/withPackagingOptions.js
+++ examples/react-native-expo/plugins/withPackagingOptions.js
@@ -6,45 +6,36 @@ const { withProjectBuildGradle } = require('expo/config-plugins')
  *
  * The fix must be applied to ALL subprojects (not just :app) because library modules
  * like react-native-gesture-handler also build test APKs that encounter the same conflict.
+ *
+ * We use allprojects with afterEvaluate at the TOP of build.gradle so it registers
+ * before any project evaluation happens.
  */
 const withPackagingOptions = (config) => {
   return withProjectBuildGradle(config, (config) => {
     if (config.modResults.language === 'groovy') {
-      const subprojectsBlock = `
-subprojects { subproject ->
-    subproject.plugins.withId('com.android.library') {
-        subproject.android {
-            packagingOptions {
-                pickFirst 'lib/arm64-v8a/libfbjni.so'
-                pickFirst 'lib/armeabi-v7a/libfbjni.so'
-                pickFirst 'lib/x86/libfbjni.so'
-                pickFirst 'lib/x86_64/libfbjni.so'
-                pickFirst 'lib/arm64-v8a/libc++_shared.so'
-                pickFirst 'lib/armeabi-v7a/libc++_shared.so'
-                pickFirst 'lib/x86/libc++_shared.so'
-                pickFirst 'lib/x86_64/libc++_shared.so'
-            }
-        }
-    }
-    subproject.plugins.withId('com.android.application') {
-        subproject.android {
-            packagingOptions {
-                pickFirst 'lib/arm64-v8a/libfbjni.so'
-                pickFirst 'lib/armeabi-v7a/libfbjni.so'
-                pickFirst 'lib/x86/libfbjni.so'
-                pickFirst 'lib/x86_64/libfbjni.so'
-                pickFirst 'lib/arm64-v8a/libc++_shared.so'
-                pickFirst 'lib/armeabi-v7a/libc++_shared.so'
-                pickFirst 'lib/x86/libc++_shared.so'
-                pickFirst 'lib/x86_64/libc++_shared.so'
+      const allprojectsBlock = `allprojects {
+    afterEvaluate { project ->
+        if (project.hasProperty('android')) {
+            project.android {
+                packagingOptions {
+                    pickFirst 'lib/arm64-v8a/libfbjni.so'
+                    pickFirst 'lib/armeabi-v7a/libfbjni.so'
+                    pickFirst 'lib/x86/libfbjni.so'
+                    pickFirst 'lib/x86_64/libfbjni.so'
+                    pickFirst 'lib/arm64-v8a/libc++_shared.so'
+                    pickFirst 'lib/armeabi-v7a/libc++_shared.so'
+                    pickFirst 'lib/x86/libc++_shared.so'
+                    pickFirst 'lib/x86_64/libc++_shared.so'
+                }
             }
         }
     }
 }
+
 `
-      // Add subprojects block at the end of the root build.gradle
+      // Prepend allprojects block at the very beginning of the root build.gradle
       if (!config.modResults.contents.includes("pickFirst 'lib/arm64-v8a/libfbjni.so'")) {
-        config.modResults.contents += subprojectsBlock
+        config.modResults.contents = allprojectsBlock + config.modResults.contents
       }
     }
     return config
