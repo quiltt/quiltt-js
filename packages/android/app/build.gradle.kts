bbc82431afc1b2e944c568998ba7b635a4eb6692
zubairaziz <zubairaziz.dev@gmail.com>
2026-05-15 05:51:41 +0800

    Update build.gradle.kts

--- packages/android/app/build.gradle.kts
+++ packages/android/app/build.gradle.kts
@@ -33,6 +33,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/INDEX.LIST"
+        }
+    }
 }
 
 dependencies {
--- packages/android/app_jetpack_compose/build.gradle.kts
+++ packages/android/app_jetpack_compose/build.gradle.kts
@@ -43,6 +43,7 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/INDEX.LIST"
         }
     }
 }
