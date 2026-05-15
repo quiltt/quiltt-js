29507cc6d0e96a77ba7aedb1b6cf52138fdd4a24
zubairaziz <zubairaziz.dev@gmail.com>
2026-05-15 22:13:55 +0800

    Update lockfile

--- packages/android/app/build.gradle.kts
+++ packages/android/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/INDEX.LIST"
+            excludes += "/META-INF/io.netty.versions.properties"
         }
     }
 }
--- packages/android/app_jetpack_compose/build.gradle.kts
+++ packages/android/app_jetpack_compose/build.gradle.kts
@@ -44,6 +44,7 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
             excludes += "/META-INF/INDEX.LIST"
+            excludes += "/META-INF/io.netty.versions.properties"
         }
     }
 }
